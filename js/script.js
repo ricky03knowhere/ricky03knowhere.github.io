9800ec0bb6064f723cab41b33d1177361ec0922b
ricky03knowhere <67403273+ricky03knowhere@users.noreply.github.com>
2020-11-27 15:14:41 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+    $(document).ready(function() {
+      $(window).scroll(function() {
+        var nScroll = $(this).scrollTop();
+        if (nScroll > 250) {
+          $(".navbar").addClass("nav");
+          $(".navbar-brand").removeClass("shad");
+          $(".nav-item").removeClass("shad");
+
+        } else if (nScroll < 250) {
+          $(".navbar").removeClass("nav");
+          $(".navbar-brand").addClass("shad");
+          $(".nav-item").addClass("shad");
+        }
+      })
+
+
+    })
+
+    $(".scroll").on("click", function(e) {
+      // e.preventDefault()
+      const link = $(this).attr("href")
+      const dest = $(link).offset().top - 100
+
+      console.log(dest)
+      $("#page-top").animate({
+        scrollTop: dest
+      },
+        1250)
+      e.preventDefault
+
+    })
