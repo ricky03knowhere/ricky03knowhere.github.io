4b228418d833bfa4cc4caef5ad4d740063c0bb60
ricky03knowhere <ricky03senju@gmail.com>
2021-02-28 15:05:53 +0700

    create certificates section

---
+++ js/certificates.js
@@ -0,0 +1,63 @@
+$('#certificates .slides').hiSlide();
+
+function pictChange(i) {
+  let title = $('#certificates .contain .item-'+i).data('title')
+  let year = $('#certificates .contain .item-'+i).data('year')
+
+  $('#certificates .preview .card').css({
+    "background-image": "url(img/certificates/"+i+".png)"
+  })
+
+  $('#certificates .preview h3 ,#certificates .preview .year').toggleClass('emerge')
+  setTimeout(() => {
+    $('#certificates .preview h3, #certificates .preview .year').toggleClass('wipe')
+
+    $('#certificates .preview h3').text(title)
+    $('#certificates .preview .year').text(year)
+
+  }, 500)
+
+  setTimeout(() => {
+    $('#certificates .preview h3, #certificates .preview .year').toggleClass('emerge')
+
+    $('#certificates .preview h3, #certificates .preview .year').toggleClass('wipe')
+  }, 700)
+
+
+}
+
+
+
+$('#certificates .contain .card').click((e) => {
+  $('#certificates .preview').toggleClass('view')
+
+  let index = $('#certificates .contain').children()
+
+  let i = e.target.attributes.index.value
+
+
+  $('#certificates .preview .prev').click(() => {
+    if (i == 0)
+      i = index.length
+    else
+      i--
+
+    pictChange(i)
+  })
+  $('#certificates .preview .next').click(() => {
+    if (i == index.length)
+      i = 1
+    else
+      i++
+
+    pictChange(i)
+  })
+
+  pictChange(i)
+})
+
+
+$('#certificates .close').click(() => {
+  $('#certificates .preview').toggleClass('view')
+
+})
