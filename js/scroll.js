e28b8643a5d52620e9b407de19fe23a6eeeabf37
ricky03knowhere <ricky03senju@gmail.com>
2021-03-05 15:56:52 +0700

    create certificates page & waved section

--- js/scroll.js
+++ js/scroll.js
@@ -1,31 +1,35 @@
-    $(document).ready(function() {
-      $(window).scroll(function() {
-        var nScroll = $(this).scrollTop();
-        if (nScroll > 250) {
-          $(".navbar").addClass("nav");
-          $(".navbar-brand").removeClass("shad");
-          $(".nav-item").removeClass("shad");
+$(document).ready(function() {
+  $(window).scroll(function() {
+    var nScroll = $(this).scrollTop();
+    if (nScroll > 250) {
+      $(".navbar").addClass("nav");
+      $(".navbar-brand").removeClass("shad");
+      $(".nav-item").removeClass("shad");
 
-        } else if (nScroll < 250) {
-          $(".navbar").removeClass("nav");
-          $(".navbar-brand").addClass("shad");
-          $(".nav-item").addClass("shad");
-        }
-      })
+    } else if (nScroll < 250) {
+      $(".navbar").removeClass("nav");
+      $(".navbar-brand").addClass("shad");
+      $(".nav-item").addClass("shad");
+    }
+  })
 
 
-    })
+})
 
-    $(".scroll").on("click", function(e) {
-      // e.preventDefault()
-      const link = $(this).attr("href")
-      const dest = $(link).offset().top - 100
+$(".scroll").on("click", function(e) {
+  // e.preventDefault()
+  const link = $(this).attr("href")
+  const dest = $(link).offset().top - 100
 
-      console.log(dest)
-      $("#page-top").animate({
-        scrollTop: dest
-      },
-        1250)
-      e.preventDefault
+  $("#page-top").animate({
+    scrollTop: dest
+  },
+    1250)
+  e.preventDefault
 
-    })
+})
+
+
+let date = new Date()
+let year = date.getFullYear()
+$('footer #year').html(year)
